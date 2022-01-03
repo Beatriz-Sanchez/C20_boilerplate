3c913d8621536e451c800cb3c7f0b0e9d7c39d95
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-01-02 21:44:08 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,45 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
 
+var engine, world;
+var bola, solo;
 
 function setup() {
   createCanvas(400,400);
+  
+  //motor e mundo
+  engine = Engine.create();
+  world = engine.world;
+  
+  //corpo da bola
+  var bola_options = {
+    restitution: 1,
+    frictionAir: 0.005
+  }
+  bola = Bodies.circle(100,50,25, bola_options);
+  World.add(world, bola);
+  
+  //corpo do solo
+  var solo_options = {
+    isStatic: true
+  }
+  solo = Bodies.rectangle(200,400,400,50, solo_options);
+  World.add(world, solo);
 }
 
 function draw() {
   background(0);
+  
+  //atualizando motor
+  Engine.update(engine);
+  
+  //desenho da bola
+  ellipseMode(RADIUS);
+  ellipse(bola.position.x,bola.position.y,25);
+  
+  //desenho do solo
+  rectMode(CENTER);
+  rect(solo.position.x,solo.position.y,400,50);
 }
 
